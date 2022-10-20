8c98edb11083fd512587385db47e7fce1fa8cae6
unknown <qarshiyevnavruz7777@gmil.com>
2022-10-20 20:17:08 +0500

    7 v

--- if07.py
+++ if07.py
@@ -13,4 +13,18 @@ def main(temp):
     Returns:
         str: return answer.
     """
-    return
+    s=0
+    if temp<0:
+        s="Freezing"
+    if temp>=1 and temp<11:
+        s="Very Cold"
+    if temp>=11 and temp<21:
+        s="Cold"
+    if temp>=21 and temp<31:
+        s="Normal"
+    if temp>=31 and temp<40:
+        s="Hot"
+    if temp>40:
+        s="very hot"
+    return s
+print(main(21)) 
