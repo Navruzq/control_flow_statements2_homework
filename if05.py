0c7d6c2d70a12303d07a1873dd350752a9ee18d7
unknown <qarshiyevnavruz7777@gmil.com>
2022-10-20 19:24:03 +0500

    5 v

--- if05.py
+++ if05.py
@@ -6,4 +6,27 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    n=n//10
+
+    x2=n%10
+    n=n//10
+
+    x3=n%10
+    n=n//10
+
+    x4=n%10
+    n=n//10
+
+    x5=n%10
+    n=n//10
+   
+    m=x1
+    if m<x2:
+        m=x2
+    if m<x3:
+        m=x3
+    if m<x4:
+        m=x4
+    return m
+print(main(12345))
